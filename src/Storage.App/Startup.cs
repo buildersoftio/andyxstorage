bcad2b68ae80321fdc3dfc4fcfbda14cd9be3751
Enes Hoxha <eneshoxha@outlook.com>
2021-05-28 17:28:12 +0200

    Remove Swashbuckle lib

--- src/Storage.App/Startup.cs
+++ src/Storage.App/Startup.cs
@@ -3,9 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Serilog;
 using System;
 
