a78e6d08f9788faea7dbac56595a8790d54bc3c7
Enes Hoxha <eneshoxha@outlook.com>
2021-10-10 22:02:12 +0200

    updated: updated brand logo

--- src/Storage.IO/Services/SystemIOService.cs
+++ src/Storage.IO/Services/SystemIOService.cs
@@ -1,5 +1,6 @@
 ﻿using Buildersoft.Andy.X.Storage.IO.Locations;
 using Microsoft.Extensions.Logging;
+using System;
 using System.IO;
 
 namespace Buildersoft.Andy.X.Storage.IO.Services
@@ -8,8 +9,25 @@ namespace Buildersoft.Andy.X.Storage.IO.Services
     {
         public SystemIOService(ILogger<SystemIOService> logger)
         {
-            logger.LogInformation("Buildersoft");
-            logger.LogInformation("Buildersoft Andy X Storage");
+            var generalColor = Console.ForegroundColor;
+            Console.WriteLine("                   Starting Buildersoft Andy X Storage");
+            Console.WriteLine("                   Copyright (C) 2021 Buildersoft LLC");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("  ###"); Console.ForegroundColor = generalColor; Console.WriteLine("      ###");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("    ###"); Console.ForegroundColor = generalColor; Console.Write("  ###");
+            Console.WriteLine("       Andy X Storage 2.0.3-preview. Copyright (C) 2021 Buildersoft LLC");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("      ####         "); Console.ForegroundColor = generalColor; Console.WriteLine("Licensed under the Apache License 2.0.  See https://bit.ly/3DqVQbx");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("    ###  ###");
+            Console.Write("  ###      ###     "); Console.ForegroundColor = generalColor; Console.WriteLine("Andy X Storage is an open-source standalone service that is used to store messages for Andy X. The Storage service is offers support for Multitenancy storage. Andy X Storage hosts all messages and makes sure that all of them are readable for the client");
+            Console.WriteLine("");
+
+
+            Console.WriteLine("                   Starting Buildersoft Andy X Storage...");
+            Console.WriteLine("\n");
             logger.LogInformation("ANDYX-STORAGE#READY");
         }
 
