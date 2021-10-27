68d789d224cf4d4de6872ca4619b686bfb310609
Granit Xheladini <granit.xh@gjirafa.com>
2021-10-27 14:31:14 +0200

    feature/38 Add FileReader
    Moved logic of Bach storing from ConsumerIOService to ConsumerConnector

---
+++ src/Storage.IO/Readers/FileReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Buildersoft.Andy.X.Storage.IO.Readers
+{
+    public static class FileReader
+    {
+        public static string[] TryReadAllLines(string path)
+        {
+            try
+            {
+                return File.ReadAllLines(path);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
